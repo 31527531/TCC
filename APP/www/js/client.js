9411a67854acff9d9c317c4ba00881177791675a
31527531 <marcos.marcelino1@hotmail.com>
2018-09-24 20:24:16 -0300

    Add files via upload

--- APP/www/js/client.js
+++ APP/www/js/client.js
@@ -1,18 +1,18 @@
 var http = require('http');
 var querystring = require('querystring');
-var cors = require('cors')
 
 var postData = querystring.stringify({
     msg: 'hello world'
 });
 
 var options = {
-    hostname: 'localhost',
+    hostname: 'https://enchantingveneratedobjects--five-nine.repl.co/',
     port: 8080,
     method: 'POST',
     headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
         'Content-Length': postData.length
+        
     }
 };
 
@@ -21,7 +21,9 @@ var req = http.request(options, function (res) {
     console.log('HEADERS:', JSON.stringify(res.headers));
 
     res.setEncoding('utf8');
-
+    
+    
+    
     res.on('data', function (chunk) {
         console.log('BODY:', chunk);
     });
@@ -30,6 +32,8 @@ var req = http.request(options, function (res) {
         console.log('No more data in response.');
     });
 });
+
+
 
 req.on('error', function (e) {
     console.log('Problem with request:', e.message);
